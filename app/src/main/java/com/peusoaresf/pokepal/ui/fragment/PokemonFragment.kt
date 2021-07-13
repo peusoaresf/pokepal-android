4a8a637ddf5b0721be8203019605d93e9cbf8ccc
Pedro Ferraz <psoaresferraz@gmail.com>
2021-07-13 18:05:36 -0300

    feat: fix pokemon layout on small screens

--- app/src/main/java/com/peusoaresf/pokepal/ui/fragment/PokemonFragment.kt
+++ app/src/main/java/com/peusoaresf/pokepal/ui/fragment/PokemonFragment.kt
@@ -15,16 +15,12 @@ import com.peusoaresf.pokepal.databinding.FragmentPokemonBinding
 import com.peusoaresf.pokepal.ui.adapter.PokemonDetailsPagerAdapter
 
 class PokemonFragment : Fragment() {
-    private lateinit var binding: FragmentPokemonBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentPokemonBinding.inflate(inflater)
+        val binding = FragmentPokemonBinding.inflate(inflater)
         val selectedPokemon = PokemonFragmentArgs.fromBundle(requireArguments()).pokemon
-
-        handleOrientationChange(this.resources.configuration)
 
         binding.lifecycleOwner = viewLifecycleOwner
         binding.pokemon = selectedPokemon
@@ -39,32 +35,5 @@ class PokemonFragment : Fragment() {
         }.attach()
 
         return binding.root
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-
-        handleOrientationChange(newConfig)
-    }
-
-    // TODO: find a better way to deal with different orientations.
-    // Maybe create different layout files for screens that aren't as responsive by default?
-    private fun handleOrientationChange(newConfig: Configuration) {
-        val viewPagerLayoutParams = binding.viewPagerDetails.layoutParams as ConstraintLayout.LayoutParams
-
-        when (newConfig.orientation) {
-            Configuration.ORIENTATION_PORTRAIT -> {
-                viewPagerLayoutParams.bottomToBottom = binding.constraintLayoutPokemon.id
-                binding.viewPagerDetails.minimumHeight = 0
-            }
-            Configuration.ORIENTATION_LANDSCAPE -> {
-                viewPagerLayoutParams.bottomToBottom = ConstraintLayout.LayoutParams.UNSET
-                binding.viewPagerDetails.minimumHeight = TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP, 250f, resources.displayMetrics
-                ).toInt()
-            }
-        }
-
-        binding.viewPagerDetails.layoutParams = viewPagerLayoutParams
     }
 }
