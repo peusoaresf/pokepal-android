78e85dca4e374981c73c80fc6202c72271510343
Pedro Ferraz <psoaresferraz@gmail.com>
2021-07-26 01:47:16 -0300

    feat: add database preload from asset

--- app/src/main/java/com/peusoaresf/pokepal/database/Database.kt
+++ app/src/main/java/com/peusoaresf/pokepal/database/Database.kt
@@ -5,9 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.peusoaresf.pokepal.database.dao.PokemonDao
-import com.peusoaresf.pokepal.database.entity.PokemonEntity
+    import com.peusoaresf.pokepal.database.entity.PokemonEntity
 
-@Database(entities = [PokemonEntity::class], version = 1)
+    @Database(entities = [PokemonEntity::class], version = 1)
 abstract class PokepalDatabase: RoomDatabase() {
     abstract val pokemonDao: PokemonDao
 }
@@ -17,11 +17,9 @@ private lateinit var INSTANCE: PokepalDatabase
 fun getDatabase(context: Context): PokepalDatabase {
     synchronized(PokepalDatabase::class.java) {
         if (!::INSTANCE.isInitialized) {
-            INSTANCE = Room.databaseBuilder(
-                context.applicationContext,
-                PokepalDatabase::class.java,
-                "Pokepal"
-            ).build()
+            INSTANCE = Room.databaseBuilder(context.applicationContext, PokepalDatabase::class.java,"Pokepal")
+                .createFromAsset("database/Pokepal.db")
+                .build()
         }
     }
     return INSTANCE
